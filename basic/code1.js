e49f03a5a2e680a3f8915f2cdd701c132f8a4721
Sam805-debug <singhsmeer805@gmail.com>
2025-09-22 08:38:04 +0000

    container config done

---
+++ basic/code1.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="sameer@gmail.com"
+var accountpassword=12345
+accountcity="jaipur"
+
+accountEmail="priya@gmail.com"
+console.table([accountId,accountEmail,accountpassword,accountcity])
+console.log(accountEmail)
