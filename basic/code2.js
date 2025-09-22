1bf0f8166d9211db3b4e90fa710ba20a191c70ff
Sam805-debug <singhsmeer805@gmail.com>
2025-09-22 09:20:21 +0000

    container config done

---
+++ basic/code2.js
@@ -0,0 +1,18 @@
+// DATA TYPES AND EMCA
+ console.log(3+3)
+ console.log("sameer")
+
+ //data types in js
+
+ let name="sameer"     //string=>""
+ let age=21           // variable
+ let logIn=true      // boolean =>true or false
+ let state;         //undefined =>value is not assign
+ let city=null    //null =>standalone value
+
+//symbol=>uniqe
+//object
+
+ console.table([name,age,logIn,state,city])
+ console.table(typeof [name,age,logIn])
+ console.table(typeof logIn)
