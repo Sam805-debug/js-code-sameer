5c2260b3205a1fb54e10d52fa6de2d1228f1b3a3
Sam805-debug <singhsmeer805@gmail.com>
2025-09-25 19:30:10 +0000

    container config done

---
+++ basic/code3.js
@@ -0,0 +1,15 @@
+//DATA TYPE CONVERSATION 
+let a = "33abc"
+console.log(typeof a)   // string
+
+let b = Number(a)
+console.log(typeof b)   // number
+console.log(b)          // NaN
+
+
+let c= undefined
+console.log(typeof c)  //undefined
+
+let d= Number(c)       
+console.log(typeof d)  //numder
+console.log(d)          //NaN
